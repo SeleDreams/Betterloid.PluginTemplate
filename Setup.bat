69366019eecb9fb738af61e3ac9cbb5d5caded56
SeleDreams <seledreams.contact@gmail.com>
2021-05-07 07:01:48 +0200

    Updated Setup.bat to fix bugs when started outside cmd.exe

--- Setup.bat
+++ Setup.bat
@@ -1,6 +1,6 @@
 @echo off
+pushd %~dp0
 goto check_Permissions
-
 :check_Permissions
     net session >nul 2>&1
     if %errorLevel% == 0 (
@@ -10,18 +10,21 @@ goto check_Permissions
         pause >nul
         exit /b 1
     )
-
 :make_link
-    set /p location="Please Specify the location of your VOCALOID5 installation folder (MAKE SURE TO USE QUOTING MARKS): "
+    rmdir VOCALOID5
+    set /p location="Please Specify the location of your VOCALOID5 installation folder (MAKE SURE TO USE QUOTING MARKS):"
     if %location% == "" (
         echo "The input is invalid !"
+        pause >nul
         exit /b 1
     )
     mklink /J VOCALOID5 %location%
     if %errorLevel% == 0 (
         echo "Succesfully created symlink, you can now start working on your plugin"
+        pause >nul
         exit /b 0
     ) else (
         echo "An error occured when making the symlink, make sure there are no errors in your path and that the symlink doesn't already exist"
+        pause >nul
         exit /b 1
     )
